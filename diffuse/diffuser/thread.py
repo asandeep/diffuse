fed1b189f77c6066cdfe187f51557d509c5f86d1
Sandeep Aggarwal <asandeep.me@gmail.com>
2020-08-22 19:12:49 +0530

    Moved task to thread diffuser.

--- diffuse/diffuser/thread.py
+++ diffuse/diffuser/thread.py
@@ -10,8 +10,34 @@ from diffuse.worker import thread as thread_worker
 LOGGER = logging.getLogger(__name__)
 
 
-class ThreadDiffuser(base._BaseDiffuser):
+class _Task(object):
+    def __init__(self, target, *args, **kwargs):
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs
+        self.future = futures.Future()
+
+    def run(self):
+        if not self.future.set_running_or_notify_cancel():
+            return
+
+        try:
+            result = self.target(*self.args, **self.kwargs)
+        except BaseException as exc:
+            # A catch-all block to prevent thread from getting killed abruptly
+            # in event of an exception.
+            LOGGER.error("Error while running target: %s", str(exc))
+            self.future.set_exception(exc)
+            # Break a reference cycle with the exception 'exc'
+            self = None
+        else:
+            self.future.set_result(result)
+
+
+class ThreadDiffuser(base._SyncDiffuser):
     _WORKER_CLASS = thread_worker.ThreadWorker
+    _TASK_CLASS = _Task
+    _QUEUE_EMPTY_EXCEPTION = thread_safe_queue.Empty
 
     def __init__(self, target, ephemeral=False, max_workers=None):
         if max_workers is None:
@@ -24,6 +50,7 @@ class ThreadDiffuser(base._BaseDiffuser):
         self._max_workers = max_workers
 
         self._task_queue = thread_safe_queue.Queue()
+        self._close_lock = threading.Lock()
 
         super().__init__(target, ephemeral)
 
@@ -35,11 +62,6 @@ class ThreadDiffuser(base._BaseDiffuser):
     def max_workers(self):
         return self._max_workers
 
-    def _drain_tasks(self):
-        while True:
-            try:
-                task = self.task_queue.get_nowait()
-                if task is not None:
-                    task.future.cancel()
-            except thread_safe_queue.Empty:
-                break
+    @property
+    def close_lock(self):
+        return self._close_lock
