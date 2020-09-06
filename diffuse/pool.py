f63009afb8bd1d1144501ce33c4bbc63fd8876b8
Sandeep Aggarwal <asandeep.me@gmail.com>
2020-09-06 17:42:43 +0530

    Use weak references to maintain list of workers in pool.

--- diffuse/pool.py
+++ diffuse/pool.py
@@ -1,4 +1,5 @@
 import logging
+import weakref
 
 LOGGER = logging.getLogger(__name__)
 
@@ -7,18 +8,21 @@ class WorkerPool:
     """Worker pool that keeps track of currently running worker processes."""
 
     def __init__(self):
-        self._workers = []
+        # Keep week ref for workers added to the pool. This simplifies pool
+        # logic as it is no longer required to keep track of dead workers.
+        self._workers = weakref.WeakSet()
 
     @property
     def size(self):
-        """The current pool size.
+        """
+        The current pool size.
 
         Returns the count of workers that are currently alive.
         """
-        self._workers = [
-            worker for worker in self._workers if worker.is_alive()
-        ]
-        return len(self._workers)
+        # Weekrefs are maintained till they are garbage collected. Simply
+        # returning the length of pool will give incorrect result in case worker
+        # is dead but not yet garbage collected.
+        return len([worker.is_alive() for worker in self._workers])
 
     def add(self, worker):
         """Adds a new worker to pool.
@@ -26,7 +30,7 @@ class WorkerPool:
         Args:
             worker: The implementation specific worker instance.
         """
-        self._workers.append(worker)
+        self._workers.add(worker)
 
     def shutdown(self, wait):
         """Shuts down pool and stops all the worker processes.
@@ -45,7 +49,7 @@ class WorkerPool:
         # Wait for worker processes to complete. Due to the way worker processes
         # decides that it is time to shutdown, joining of worker can't be done
         # just after workers are asked to stop above.
-        for task_worker in list(self._workers):
+        for task_worker in self._workers:
             task_worker.join()
             LOGGER.debug("Stopped worker: %s", task_worker.id)
 
@@ -58,7 +62,7 @@ class WorkerPool:
         if not wait:
             return
 
-        for task_worker in list(self._workers):
+        for task_worker in self._workers:
             await task_worker.join()
             LOGGER.debug("Stopped worker: %s", task_worker.id)
 
@@ -66,11 +70,13 @@ class WorkerPool:
         """Stops running worker processes."""
         LOGGER.debug("Shutting down worker pool.")
 
-        # When using ephemeral workers, the workers might have already stopped
-        # when this method is called. If that is the case, return.
-        if not self.size:
-            return
-
-        for task_worker in list(self._workers):
+        # Since task queue is shared among workers, stop signal sent to a worker
+        # might actually be read by completely different worker. This will
+        # result in the receiving worker initiating its termination process and
+        # getting auto evicted from pool due to weak reference.
+        # To make sure every worker receives stop signal at least once, we need
+        # to send as many signals as there are workers in pool.
+        workers_copy = [worker for worker in self._workers]
+        for task_worker in workers_copy:
             LOGGER.debug("Stopping worker: %s", task_worker.id)
             task_worker.stop()
